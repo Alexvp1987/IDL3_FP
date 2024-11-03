74d9e4105f7eca3ae033378dd54546a74b78c9aa
Alexvp1987 <i2422060@continental.edu.pe>
2024-11-03 00:50:24 -0500

    ejercicio11

--- Ejercicios_IDL3_FP.py
+++ Ejercicios_IDL3_FP.py
@@ -207,3 +207,40 @@ st.write(f"Número de notas desaprobadas: {desaprobados}")
 st.write(f"Promedio de las notas: {promedio:.2f}")
 
 st.write("********************************************************************************************")
+
+
+#Ejercicio-11
+#Crea un programa que pida al usuario una contraseña, de forma repetitiva mientras que no 
+#introduzca “asdasd”. Cuando finalmente escriba la contraseña correcta, se le dirá “Bienvenido” y 
+#terminará el programa.
+
+st.subheader("Ejercicio 11: Login")
+
+def verificar_contrasena():
+    st.title("Verificación de contraseña")
+
+    # Contraseña correcta
+    contrasena_correcta = "asdasd"
+
+    # Inicializar variable de sesión para almacenar la entrada del usuario
+    if "contrasena_ingresada" not in st.session_state:
+        st.session_state.contrasena_ingresada = ""
+
+    # Input de la contraseña
+    contrasena_ingresada = st.text_input("Introduce la contraseña:", type="password")
+
+    # Botón para verificar la contraseña
+    if st.button("Verificar"):
+        if contrasena_ingresada == contrasena_correcta:
+            st.session_state.contrasena_ingresada = contrasena_ingresada
+            st.success("Bienvenido")
+        else:
+            st.error("Contraseña incorrecta, intenta de nuevo")
+
+# Llamada a la función principal
+if __name__ == "__main__":
+    verificar_contrasena()
+
+
+st.write("********************************************************************************************")
+
