5d714e2ae225d2bbd74b68c145159728d5f006ec
Alexvp1987 <i2422060@continental.edu.pe>
2024-11-02 23:14:58 -0500

    corrigiendo 2

--- Ejercicios_IDL3_FP.py
+++ Ejercicios_IDL3_FP.py
@@ -1,8 +1,13 @@
 #Ejercicio-01: 
-#Se requiere el código en python para imprimir en pantalla 10 veces el texto“HolaMundo”.
+#Se requiere el código en python para imprimir en pantalla 10 veces el texto “HolaMundo”.
 
 import streamlit as st
 
+st.title("Ejercicios para el IDL3")
+st.subheader("Ejercicio 1: Imprimir en pantalla 10 veces el texto HolaMundo")
+
 for i in range(10):
     st.write("HolaMundo")
+
+st.write("********************************************************************************************")
 
